b07a8939503df5b2b111b956c8f2a1dffb8384c3
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2024-03-24 22:51:00 +0700

    add User

--- lottery.sol
+++ lottery.sol
@@ -1,15 +1,37 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract lottery {
-    uint T1;
-    uint T2;
-    uint T3;
-    uint N;
+    struct User{
+        uint choice;
+        byte32 commit;
+        address addr;
+    }
+
+    uint private T1;
+    uint private T2;
+    uint private T3;
+    uint private N;
+
+    uint private stage = 1;
+    uint private startTime = 0;
 
     constructor(uint t1, uint t2, uint t3, uint n) {
         T1 = t1;
         T2 = t2;
         T3 = t3;
         N = n;
+    }
+
+    function _reset() private {
+        stage = 1;
+        startTime = 0;
+    }
+
+    function addUser() public payable {
+        require(msg.value == 1e15 wei);
+
+        if (startTime == 0) {
+            startTime = block.timestamp;
+        }
     }
 }
